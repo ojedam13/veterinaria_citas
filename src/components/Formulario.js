ee0118df8a6d2f25841cc8c9723389a4706f04af
ojedam13 <m_ojeda17@hotmail.com>
2022-03-03 19:24:35 -0300

    preventDefault para cuando el usuario envie el form

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -23,11 +23,26 @@ const Formulario = () => {
     //Extraer los valores
     const { mascota, propietario, fecha, hora, sintomas } = cita;
 
+    //Cuando el usuario presiona agregar cita
+    const submitCita = e => {
+        e.preventDefault();
+
+        // Validar
+
+        //asignar un ID
+
+        // Crear la cita
+
+        //Reiniciar el form
+    }
+
     return ( 
         <Fragment>
             <h3>Crear Cita</h3>
 
-            <form>
+            <form
+                onSubmit={submitCita}
+            >
                 <label>Nombre Mascota</label>
                 <input 
                     type='text'
