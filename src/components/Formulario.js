42be73e2432cc6676b1de2d53c50ae732c597fc0
ojedam13 <m_ojeda17@hotmail.com>
2022-03-04 17:57:41 -0300

    reiniciar form

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -43,6 +43,13 @@ const Formulario = ({crearCita}) => {
         // Crear la cita
         crearCita(cita);
         //Reiniciar el form
+        actualizarCita({
+            mascota: '',
+            propietario: '',
+            fecha: '',
+            hora: '',
+            sintomas: ''
+        })
     }
 
     return ( 
